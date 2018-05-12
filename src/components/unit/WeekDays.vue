b680d1c538179595dadc91a5df7d63ae145293ea
an9elkiss <an9elkiss@163.com>
2018-05-12 21:56:35 +0800

    update page

--- src/components/unit/WeekDays.vue
+++ src/components/unit/WeekDays.vue
@@ -5,15 +5,21 @@
       <p class="xs-mt-10 xs-mb-10">
         <template v-for="item in days">
           <button :key="item.dateNum" v-if="item.type == 1" class="btn btn-rounded btn-space btn-primary"
-                  @click="dateChange(item.date)">{{ item.dateNum }}
+                  @click="dateChange(item.date)">
+            <i v-show="item.selected" class="icon icon-left mdi mdi-check-circle"></i>
+            {{ item.dateNum }}
           </button>
           <button :key="item.dateNum" v-if="item.type == 2" class="btn btn-rounded btn-space btn-success"
-                  @click="dateChange(item.date)">{{ item.dateNum }}
+                  @click="dateChange(item.date)">
+            <i v-show="item.selected" class="icon icon-left mdi mdi-check-circle"></i>
+            {{ item.dateNum }}
           </button>
           <button :key="item.dateNum" v-if="item.type == 3" class="btn btn-rounded btn-space btn-default">{{ item.dateNum }}
           </button>
           <button :key="item.dateNum" v-if="item.type == 4" class="btn btn-rounded btn-space btn-danger"
-                  @click="dateChange(item.date)">{{ item.dateNum }}
+                  @click="dateChange(item.date)">
+            <i v-show="item.selected" class="icon icon-left mdi mdi-check-circle"></i>
+            {{ item.dateNum }}
           </button>
         </template>
       </p>
@@ -34,10 +40,16 @@ export default {
   },
   created () {
     var vm = this
+    // 组件初始化时，选中当天日期
     Bus.$on(Global.event.timeEntryWeekDaysGot, function () {
-      vm.dateChange(vm.days[0].date)
+      vm.days.forEach(item => {
+        if (item.type === 2) {
+          vm.dateChange(item.date)
+        }
+      })
     })
 
+    // 日程保存后，刷新WeekDays
     Bus.$on(Global.event.timeEntrySaved, function () {
       vm.getWeekDays()
     })
@@ -49,6 +61,15 @@ export default {
     getWeekDays (sendEvent) {
       this.$http.get(Global.url.apiWeekDays).then(response => {
         if (response.body.code === 200) {
+          response.body.data.days.forEach(item => {
+            // 获取days数据后，在数据绑定前需要先设置 selected 属性
+            if (item.type === 2) {
+              item.selected = true
+            } else {
+              item.selected = false
+            }
+          })
+
           this.days = response.body.data.days
 
           if (sendEvent) {
@@ -61,6 +82,13 @@ export default {
     },
 
     dateChange: function (date) {
+      this.days.forEach(item => {
+        if (item.date === date) {
+          item.selected = true
+        } else {
+          item.selected = false
+        }
+      })
       Bus.$emit(Global.event.timeEntryDateChange, date)
     }
   }

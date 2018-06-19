1f88e669090607a966c688857f71ef880ceb3711
潘洪雪 <hongxue.pan@baozun.com>
2018-06-19 18:08:07 +0800

    v1

--- src/components/pages/codeReview/CodeReviewDetail.vue
+++ src/components/pages/codeReview/CodeReviewDetail.vue
@@ -1,3 +1,15 @@
-<template></template>
+<template>
+  <div class="be-content codeReviewDetail">
+    <div class="main-content container-fluid">
+      <div class="panel panel-default">
+        <div class="panel-heading panel-heading-divider code-review-header center">张三的code review<span class="panel-subtitle">2018-06-19</span></div>
+      </div>
+    </div>
+  </div>
+</template>
 <script></script>
-<style></style>
+<style>
+  .codeReviewDetail .code-review-header{
+    border-bottom: none;
+  }
+</style>

697c0d3bb7a07ebb331674629a80a6748eb0d2c2
潘洪雪 <hongxue.pan@baozun.com>
2018-06-20 14:08:38 +0800

    merge

--- src/components/pages/codeReview/CodeReviewDetail.vue
+++ src/components/pages/codeReview/CodeReviewDetail.vue
@@ -4,27 +4,50 @@
       <div class="panel panel-default">
         <div class="panel-heading panel-heading-divider code-review-header center">张三的code review<span class="panel-subtitle">2018-06-19</span></div>
       </div>
-      <div class="panel panel-default">
+      <div class="panel panel-default code-review-block">
         <div class="panel-heading panel-heading-divider">编码规范</div>
         <div class="panel-body ">
-          <h2 class="font-size-14 col-45">内容</h2>
-          <p class="line-height-18 col-45 font-size-12">编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容</p>
-          <h2 class="font-size-14 col-45">得分：50分</h2>
-          <h2 class="font-size-14 col-45">备注</h2>
-          <p class="line-height-18 col-45 font-size-12">备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注</p>
+          <h2>内容</h2>
+          <p>编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容</p>
+          <h2>得分：50分</h2>
+          <h2>备注</h2>
+          <p>备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注</p>
         </div>
       </div>
+      <div class="panel panel-default code-review-block">
+        <div class="panel-heading panel-heading-divider">业务完成度</div>
+        <div class="panel-body ">
+          <h2>内容</h2>
+          <p>编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容</p>
+          <h2>得分：50分</h2>
+          <h2>备注</h2>
+          <p>备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注</p>
+        </div>
+      </div>
+      <button class="btn btn-space btn-primary btn-add">新增任务</button>
     </div>
   </div>
 </template>
 <script></script>
 <style>
-  .codeReviewDetail h2{
-    margin: 0 0 10px;
+  .code-review-header{
+    border-bottom: none;
+  }
+  .code-review-block{
+    color: #454545;
+  }
+  .code-review-block h2{
+    margin: 15px 0 0;
     padding: 0;
     font-weight: normal;
+    font-size: 14px;
   }
-  .codeReviewDetail .code-review-header{
-    border-bottom: none;
+  .code-review-block h2:first-of-type{
+    margin: 0;
+  }
+  .code-review-block p{
+    font-size: 12px;
+    line-height: 18px;
+    margin: 10px 0 0;
   }
 </style>

111d27049a2d69313f922a9d3b518730ee748bd1
潘洪雪 <hongxue.pan@baozun.com>
2018-06-20 11:09:59 +0800

    v

--- src/components/pages/codeReview/CodeReviewDetail.vue
+++ src/components/pages/codeReview/CodeReviewDetail.vue
@@ -4,11 +4,26 @@
       <div class="panel panel-default">
         <div class="panel-heading panel-heading-divider code-review-header center">张三的code review<span class="panel-subtitle">2018-06-19</span></div>
       </div>
+      <div class="panel panel-default">
+        <div class="panel-heading panel-heading-divider">编码规范</div>
+        <div class="panel-body ">
+          <h2 class="font-size-14 col-45">内容</h2>
+          <p class="line-height-18 col-45 font-size-12">编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容编码规范内容</p>
+          <h2 class="font-size-14 col-45">得分：50分</h2>
+          <h2 class="font-size-14 col-45">备注</h2>
+          <p class="line-height-18 col-45 font-size-12">备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注备注</p>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 <script></script>
 <style>
+  .codeReviewDetail h2{
+    margin: 0 0 10px;
+    padding: 0;
+    font-weight: normal;
+  }
   .codeReviewDetail .code-review-header{
     border-bottom: none;
   }

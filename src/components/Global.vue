acdfaf94a97040f32c6b5eaac8ed827f2c71602b
李申申 <shenshen.li@baozun.com>
2018-06-27 20:14:01 +0800

    update

--- src/components/Global.vue
+++ src/components/Global.vue
@@ -32,7 +32,8 @@ const url = {
   apiGetWeek: managerApiBase + '/api-super-manager/1.0.0/common/year/month/week',
   apiPutTag: managerApiBase + '/api-super-manager/1.0.0/savetag',
   apiCodeReview: managerApiBase + '/api-super-manager/1.0.0/codeReview',
-  apiUpdateCodeReview: managerApiBase + '/api-super-manager/1.0.0/codeReview/codeReviewInfo/update'
+  apiUpdateCodeReview: managerApiBase + '/api-super-manager/1.0.0/codeReview/codeReviewInfo/update',
+  apiShowConvertedWorkHours: managerApiBase + '/api-super-manager/1.0.0/task/show/converted/workhours'
 }
 
 const event = {

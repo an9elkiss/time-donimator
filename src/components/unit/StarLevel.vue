cc079ab3b157972462d34c0cfe4a80d0dba1c7c2
袁鸣 <ming.yuan@baozun.com>
2019-02-01 15:55:22 +0800

    添加评星组件

---
+++ src/components/unit/StarLevel.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <span v-for="n in max" :key="n" :class="{'mdi mdi-star-outline': n > value && showNull, 'mdi mdi-star light': n <= value}" @click="click(n)"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StarLevel',
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 5
+    },
+    editable: {
+      type: Boolean,
+      default: true
+    },
+    clearable: {
+      type: Boolean,
+      default: false
+    },
+    showNull: {
+      type: Boolean,
+      default: true
+    }
+  },
+  methods: {
+    click (num) {
+      if (!this.editable) {
+        return
+      }
+      let ployhold = num
+      if (this.clearable && num <= this.value) {
+        ployhold = num - 1
+      }
+      this.$emit('click', ployhold)
+    }
+  }
+}
+</script>
+
+<style scoped>
+div {
+  cursor: pointer;
+}
+span {
+  margin-right: 2px;
+  font-size: 20px;
+}
+span.light {
+  color: #efb514;
+}
+</style>
